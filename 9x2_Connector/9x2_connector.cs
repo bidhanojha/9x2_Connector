62fd0c88694a3b05c1db903550874e6aa8b1da5a
bidhanojha <bidhanojha@gmail.com>
2019-06-15 13:36:46 +0200

    Initial commit

---
+++ 9x2_Connector/9x2_connector.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Activities;
+using System.ComponentModel;
+using System.Data.SqlClient;
+
+
+namespace _9x2_connector
+{
+    public class StateInfoToDB : CodeActivity
+    {
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<double> StateNumber { get; set; }
+
+        [Category("Input")]
+        public InArgument<string> StateName { get; set; }
+
+        [Category("Output")]
+        public OutArgument<Boolean> Result { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var stateNumb = StateNumber.Get(context);
+            var stateName = StateName.Get(context);
+            //var result = System.Math.Pow(firstNumber + secondNumber, 2);
+
+            SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-ENFECBU\SQLEXPRESS;Initial Catalog=TestDB;Integrated Security=True;User Instance=True");
+
+            string sql = "insert into NewTable (Name, Age) values (stateName, 100)";
+
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+                Result.Set(context, true);
+            }
+            catch (System.Data.SqlClient.SqlException ex)
+            {
+                string msg = "Insert Error:";
+                msg += ex.Message;
+                Result.Set(context, false);
+            }
+            finally
+            {
+                conn.Close();
+            }
+                                                     
+        }
+    }
+}
